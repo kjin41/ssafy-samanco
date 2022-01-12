4986626ea4f3fb6626fae395e9514b258c395bf6
kjin <kjin4_1@naver.com>
2022-01-12 23:50:39 +0900

    feat: BE-회원가입 유효성 검사

--- backend-java/src/main/generated/com/ssafy/db/entity/QUser.java
+++ backend-java/src/main/generated/com/ssafy/db/entity/QUser.java
@@ -21,7 +21,7 @@ public class QUser extends EntityPathBase<User> {
 
     public final QBaseEntity _super = new QBaseEntity(this);
 
-    public final StringPath department = createString("department");
+//    public final StringPath department = createString("department");
 
     //inherited
     public final NumberPath<Long> id = _super.id;
@@ -30,9 +30,11 @@ public class QUser extends EntityPathBase<User> {
 
     public final StringPath password = createString("password");
 
-    public final StringPath position = createString("position");
+//    public final StringPath position = createString("position");
 
     public final StringPath userId = createString("userId");
+    public final StringPath email = createString("email");
+    public final StringPath phone = createString("phone");
 
     public QUser(String variable) {
         super(User.class, forVariable(variable));
